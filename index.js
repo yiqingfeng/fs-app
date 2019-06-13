981f011bc4849f8a3afd38e28904e6c6f4140e87
戴强 <daiq@fxiaoke.com>
2019-06-13 16:41:08 +0800

    引擎模板下载

--- index.js
+++ index.js
@@ -7,26 +7,36 @@ const download = require('download-git-repo');
 const ora = require('ora');
 const chalk = require('chalk');
 const symbols = require('log-symbols');
+const handlebars = require('handlebars');
 const {
     version,
 } = require('./package.json');
 
 program.version(version, '-v, --version')
-    .command('init <name>')
-    .action(name => {
+    .command('init <template> <name>')
+    .action((template, name) => {
+		console.log(template, name);
         if(fs.existsSync(name)){
             // 错误提示项目已存在，避免覆盖原有项目
             console.log(symbols.error, chalk.red(`${name}项目已存在`));
             return;
         }
-        inquirer.prompt([{
+        inquirer.prompt([
+            {
                 name: 'projectName',
                 message: '请输入项目名称'
             },
+            {
+				name: 'isApp',
+				type: 'confirm',
+                message: '该项目是否在app目录下（默认为true）'
+			},
         ]).then(answers => {
             const spinner = ora('正在下载模板...');
             spinner.start();
-            download('xxxxx#master', name, {
+            // download(`yiqingfeng/fs-app#${template}`, name, {
+			// download('github:yiqingfeng/fs-app', name, {
+			download(`direct:https://github.com/yiqingfeng/fs-app.git#${template}`, name, {
                 clone: true
             }, err => {
                 if (err) {
